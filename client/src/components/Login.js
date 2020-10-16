d71c2fe6cb50ceb1d3f575f9cfcc07845b77a95c
Fatima Rizvi <fatimarzv23@gmail.com>
2020-10-16 10:37:34 -0500

    Finished installing dependencies and created the login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,25 @@
 import React from "react";
+import axios from 'axios';
+
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const credentials = { username: 'Lambda School', password: 'i<3Lambd4'}
+
+  axios.post('http://localhost:5000/api/login', credentials)
+    .then((res) => {
+      console.log('login res: ', res)
+      window.localStorage.setItem("token", res.data.payload);
+      this.props.history.push("/protected");
+    })
+    .catch((err) => {
+      console.log('Login error: ', err)
+    });
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
